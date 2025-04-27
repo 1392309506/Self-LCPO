f180a9514aaffc05af3d6e2b65327dd9d42bce83
lty <1392309506@qq.com>
2025-04-27 20:27:23 +0800

    start to run TALE baseline

--- prompt/dataset_prompt.py
+++ prompt/dataset_prompt.py
@@ -1,30 +1,48 @@
 MATH_PROMPT= """
+First, estimate the minimum number of tokens needed to reason through the following question correctly.
+Then, reason step-by-step within this estimated budget.
+
 Ensure the response concludes with the answer in the XML format:
-<answer>your_answer_here</answer>.
+<answer>your_answer_here</answer>
 """
 
 GPQA_PROMPT ="""
+First, estimate the minimum number of tokens needed to reason through the following question correctly.
+Then, reason step-by-step within this estimated budget.
+
 Ensure the response concludes with the answer in the XML format:
-<answer>A, B, C, or D</answer>.
+<answer>A, B, C, or D</answer>. (for example <answer>A</answer>)
 """
 
 WSC_PROMPT = """
-Ensure the response concludes with the answer in the XML format: 
+First, estimate the minimum number of tokens needed to reason through the following question correctly.
+Then, reason step-by-step within this estimated budget.
+
+Ensure the response concludes with the answer in the XML format:
 <answer>A or B</answer>
 """
 
 BBH_PROMPT ="""
-Ensure the response concludes with the answer in the XML format: 
+First, estimate the minimum number of tokens needed to reason through the following question correctly.
+Then, reason step-by-step within this estimated budget.
+
+Ensure the response concludes with the answer in the XML format:
 <answer>Yes or No</answer>
 """
 
 STR_PROMPT="""
-Ensure the response concludes with the answer in the XML format: 
+First, estimate the minimum number of tokens needed to reason through the following question correctly.
+Then, reason step-by-step within this estimated budget.
+
+Ensure the response concludes with the answer in the XML format:
 <answer>Yes or No</answer>
 """
 
 
 BOOLQ_PROMPT="""
-Ensure the response concludes with the answer in the XML format: 
+First, estimate the minimum number of tokens needed to reason through the following question correctly.
+Then, reason step-by-step within this estimated budget.
+
+Ensure the response concludes with the answer in the XML format:
 <answer>[Yes or No]</answer>.
 """
