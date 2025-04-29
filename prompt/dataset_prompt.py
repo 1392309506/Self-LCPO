f1ba8d2e29f18a86c42ebee5a09dc4e216de6010
lty <1392309506@qq.com>
2025-04-29 14:03:40 +0800

    spo baseline

--- prompt/dataset_prompt.py
+++ prompt/dataset_prompt.py
@@ -1,25 +1,15 @@
 MATH_PROMPT= """
-First, estimate the minimum number of tokens needed to reason through the following question correctly.
-Then, reason step-by-step within this estimated budget.
-
-Ensure the response concludes with the answer in the XML format:
-<answer>your_answer_here</answer>
+Please analyze the question in depth and break down the problem into clear, concise steps. \nUse bullet points or numbered lists to ensure clarity and provide logical reasoning with any required calculations or deductions. \nConclude your response with the final answer formatted in XML: <answer>your_answer_here</answer>.\n
 """
 
 GPQA_PROMPT ="""
-First, estimate the minimum number of tokens needed to reason through the following question correctly.
-Then, reason step-by-step within this estimated budget.
-
+Please think step by step.
 Ensure the response concludes with the answer in the XML format:
-<answer>A, B, C, or D</answer>. (for example <answer>A</answer>)
+<answer>A, B, C, or D</answer>.
 """
 
 WSC_PROMPT = """
-First, estimate the minimum number of tokens needed to reason through the following question correctly.
-Then, reason step-by-step within this estimated budget.
-
-Ensure the response concludes with the answer in the XML format:
-<answer>A or B</answer>
+Identify who 'they' refers to in the following sentence: \"The city councilmen refused the demonstrators a permit because they advocated violence. Options: A. The city councilmen B. The demonstrators\">
 """
 
 BBH_PROMPT ="""
@@ -31,11 +21,7 @@ Ensure the response concludes with the answer in the XML format:
 """
 
 STR_PROMPT="""
-First, estimate the minimum number of tokens needed to reason through the following question correctly.
-Then, reason step-by-step within this estimated budget.
-
-Ensure the response concludes with the answer in the XML format:
-<answer>Yes or No</answer>
+present your answer in the specified XML format: <answer>[Yes or No]</answer>.\n
 """
 
 
